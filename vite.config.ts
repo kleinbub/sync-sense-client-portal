1e1184a22dea894f5c81f3eccc341e0f8de0573b
Leonardo Cunico <leonardo@cunico.net>
2024-07-11 15:56:02 +0200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ import AutoImport from "unplugin-auto-import/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "/sync-sense-client-portal/",
+	base: "https://dotnet555.github.io/sync-sense-client-portal/",
 	plugins: [
 		vue(),
 		// vueDevTools(),
